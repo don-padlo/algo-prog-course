6ff2c78c891dd6d5b93df2b779da4fc38f4f644f
Дьяченко Иван Сергеевич <idacenko605@gmail.com>
2026-09-11 00:36:15 +0300

    Lab02 task2:name initials with char

---
+++ NameInitials/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Пожалуйста,укажите ваше имя: ");
+char name = Console.ReadLine()[0];
+
+Console.WriteLine("Пожалуйста,укажите вашу фамилию: ");
+string fam = Console.ReadLine();
+
+Console.WriteLine($"Здравствуйте, {fam} {name}.");
