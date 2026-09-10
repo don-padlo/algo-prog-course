e885592c611950e8119f410445c45f2200d8ab32
Дьяченко Иван Сергеевич <idacenko605@gmail.com>
2026-09-10 19:36:45 +0300

    Lab01:variables and interpolation

--- Lab01/obj/Debug/net10.0/Lab01.AssemblyInfo.cs
+++ Lab01/obj/Debug/net10.0/Lab01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7b6b5b7541a600bb80b0e6f38b4167a74e20e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b2757cd46f2283d9e72fcbb40fefb859f25ecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
