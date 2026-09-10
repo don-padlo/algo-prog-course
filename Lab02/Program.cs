c81ecd42f87e494a7db854a21aa51a67ffa42b3d
Дьяченко Иван Сергеевич <idacenko605@gmail.com>
2026-09-10 22:48:14 +0300

    Lab02:char and decimal precision

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -4,3 +4,16 @@ Console.WriteLine("Переполнение byte");
 byte maxByte = 255;
 byte overflowed = (byte)(maxByte + 1);
 Console.WriteLine($"255 + 1 для byte = {overflowed}");
+
+
+Console.WriteLine();
+Console.WriteLine("char");
+
+char firsLetter = 'A';
+char separator = '-';
+int charAsNumber = firstLetter; //char можно неявно превратить в int - это код символа в таблице Unicode
+Console.WriteLine($"Символ: {firstLetter},разделитель: {separator}");
+Console.WriteLine($"Код символа '{firsLetter}' в Unicode {charAsNumber}");
+Console.WriteLine($"Табуляция:\tпосле таба");
+Console.WriteLine($"Перенос:\nпосле переноса");
+
