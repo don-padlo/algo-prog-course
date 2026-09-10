755a4b7caee476954919f36bd6a4e2c9b4539616
Дьяченко Иван Сергеевич <idacenko605@gmail.com>
2026-09-10 22:53:19 +0300

    Lab02:var keybord

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -9,11 +9,19 @@ Console.WriteLine($"255 + 1 для byte = {overflowed}");
 Console.WriteLine();
 Console.WriteLine("char");
 
-char firsLetter = 'A';
+char firstLetter = 'A';
 char separator = '-';
 int charAsNumber = firstLetter; //char можно неявно превратить в int - это код символа в таблице Unicode
 Console.WriteLine($"Символ: {firstLetter},разделитель: {separator}");
-Console.WriteLine($"Код символа '{firsLetter}' в Unicode {charAsNumber}");
+Console.WriteLine($"Код символа '{firstLetter}' в Unicode {charAsNumber}");
 Console.WriteLine($"Табуляция:\tпосле таба");
 Console.WriteLine($"Перенос:\nпосле переноса");
 
+Console.WriteLine();
+Console.WriteLine("var");
+
+var studentAge = 20; //компилятор вывел int
+var gpa = 4.75; //компилятор вывел double
+var fullName = "Смирнова А.С."; //компилятор вывел string
+
+Console.WriteLine($"{fullName}, возраст {studentAge}, средний балл {gpa}");
