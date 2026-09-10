1413ae02e46f124b244675795d72846adc177957
Дьяченко Иван Сергеевич <idacenko605@gmail.com>
2026-09-10 23:53:13 +0300

    Lab02 final:student questionnaire with input

---
+++ Questionnaire/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Пожалуйста,укажите ваше ФИ: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Пожалуйста,укажите название вашей группы: ");
+string group = Console.ReadLine();
+
+Console.WriteLine("Пожалуйста,укажите ваш год рождения");
+string yr = Console.ReadLine();
+int year = Convert.ToInt32(yr);
+
+Console.WriteLine("Пожалуйста,укажите ваш средний балл за прошлый семестр: ");
+string scr = Console.ReadLine();
+double score = double.Parse(scr);
+
+Console.WriteLine("Пожалуйста,укажите вашу любимую букву алфавита");
+char bukva = Console.ReadLine()[0];
+
+Console.WriteLine(" Анкета");
+Console.WriteLine($"{name}, группа {group}");
+Console.WriteLine($"Год рождения: {year}, в 2030 году вам будет {2030 - year}");
+Console.WriteLine($"Средний балл: {score}");
+Console.WriteLine($"Ваша любимая буква - {bukva}");
